500770a338866b78ca37d89a2abedfdc9f8d59ac
Diana Shyrokikh <diashiroks@gmail.com>
2024-01-21 13:40:48 +0200

    :sparkles: Created a new addon module

---
+++ addons/library_app/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Library Management",
+    "version": "16.0.1.0.0",
+    "summary": "Manage library catalog and book lending",
+    "depends": [
+        "base",
+    ],
+    "license": "AGPL-3",
+    "application": True,
+    "sequence": -200,
+    "author": "Diana Shyrokikh",
+    "website": "https://github.com/PacktPublishing"
+               "/Odoo-15-Development-Essentials",
+    "category": "Services/Library",
+}
