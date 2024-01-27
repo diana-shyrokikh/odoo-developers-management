b26ee71c936e12ea248b64577b4545a3adb1ad46
Diana Shyrokikh <diashiroks@gmail.com>
2024-01-27 13:42:57 +0200

    :sparkles: Extended the Library app

---
+++ addons/library_member/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Library Members",
+    "version": "16.0.1.0.0",
+    "summary": "Manage members borrowing books.",
+    "depends": ["library_app"],
+    "license": "AGPL-3",
+    "application": False,
+    "sequence": -199,
+    "author": "Diana Shyrokikh",
+    "website": "https://github.com/PacktPublishing"
+               "/Odoo-15-Development-Essentials",
+    "category": "Services/Library",
+}
