0b311af1c351dcef848caf65699f1cf594fdd565
Diana Shyrokikh <diashiroks@gmail.com>
2024-01-27 14:18:57 +0200

    :sparkles: Extended Python methods to add features to the business logic

--- addons/library_member/models/library_book.py
+++ addons/library_member/models/library_book.py
@@ -13,3 +13,33 @@ class Book(models.Model):
     publisher_id = fields.Many2one(
         index=True
     )
+
+    def _check_isbn(self):
+        self.ensure_one()
+
+        digits = [
+            int(x)
+            for x
+            in self.isbn
+            if x.isdigit()
+        ]
+
+        if len(digits) == 10:
+            ponderators = [
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+            ]
+
+            total = sum(
+                a * b
+                for a, b
+                in zip(
+                    digits[:9],
+                    ponderators
+                )
+            )
+            check = total % 11
+
+            return digits[-1] == check
+
+        else:
+            return super()._check_isbn()
