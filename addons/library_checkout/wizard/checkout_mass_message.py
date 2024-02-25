b08e2f54b6ec0bf0a8945fbfb0a11acf6dd5acd3
Diana Shyrokikh <diashiroks@gmail.com>
2024-02-25 14:17:59 +0200

    :sparkles: Added exceptions to the wizard

--- addons/library_checkout/wizard/checkout_mass_message.py
+++ addons/library_checkout/wizard/checkout_mass_message.py
@@ -16,15 +16,23 @@ class CheckoutMassMessage(models.TransientModel):
     def default_get(self, field_names):
         defaults_dict = super().default_get(field_names)
         checkout_ids = self.env.context["active_ids"]
-        print("checkout_ids", checkout_ids)
         defaults_dict["checkout_ids"] = self.env["library.checkout"].browse(checkout_ids)
-        print("defaults_dict", defaults_dict)
         # Add values to the defaults_dict here
         return defaults_dict
 
 
     def button_send(self):
         self.ensure_one()
+
+        if not self.checkout_ids:
+            raise exceptions.UserError(
+                "No Checkouts were selected."
+            )
+        if not self.message_body:
+            raise exceptions.UserError(
+                "A message body is required"
+            )
+
         for checkout in self.checkout_ids:
             checkout.message_post(
                 body=self.message_body,
