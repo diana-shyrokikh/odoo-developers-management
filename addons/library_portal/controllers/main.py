3ed01532531580c4dfd7677322f5c1a93d7f73c8
Diana Shyrokikh <diashiroks@gmail.com>
2024-03-16 13:50:20 +0200

    :sparkles: Added /library/catalog/json route

--- addons/library_portal/controllers/main.py
+++ addons/library_portal/controllers/main.py
@@ -1,3 +1,5 @@
+import json
+
 from odoo import http
 
 
@@ -14,4 +16,32 @@ class Main(http.Controller):
         return http.request.render(
             "library_portal.book_catalog",
             {"books": books},
+        )
+
+class MainJSON(http.Controller):
+    @http.route(
+        "/library/catalog/json",
+        auth="public",
+        website=False
+    )
+    def catalog(self, **kwargs):
+        book = http.request.env["library.book"]
+        books = book.sudo().search([])
+
+        # Convert books to a list of dictionaries
+        books_data = []
+        for book in books:
+            books_data.append({
+                'id': book.id,
+                'name': book.name,
+                # Add other fields as needed
+            })
+
+        # Convert the dictionary to a JSON string
+        response_json = json.dumps({"books": books_data})
+
+        # Return the JSON response
+        return http.Response(
+            response_json,
+            content_type='application/json;charset=utf-8'
         )
