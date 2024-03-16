1e627f664b4cefc352f0b83a9ff022b6fba520ed
Diana Shyrokikh <diashiroks@gmail.com>
2024-03-16 13:14:05 +0200

    :sparkles: Created a frontend page

---
+++ addons/library_portal/__manifest__.py
@@ -0,0 +1,19 @@
+{
+"name": "Library Portal",
+"description": "Portal for library members",
+"author": "Diashiro",
+"license": "AGPL-3",
+"depends": [
+    "library_checkout",
+    "portal",
+],
+"data": [
+    # "security/library_security.xml",
+    # "security/ir.model.access.csv",
+
+    # "views/checkout_portal_templates.xml",
+
+    "views/main_templates.xml",
+],
+
+}
